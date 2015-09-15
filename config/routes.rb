8e350c4058eb638b66ba597a4cecb95957eac2e3
jaym <jmpaaak@gmail.com>
2015-09-15 09:44:44 +0000

    라우트 지정하기(hello-컨트롤러의 액션, hello.html.erb-뷰)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
+Mvc::Application.routes.draw do
+	get 'jaym_home/hello' => 'jaym_home#hello'
+
 Testapp::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
